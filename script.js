d8c88a296206b4f466cafc6273919cf02768e259
duc-nq <108884348+duc-nq@users.noreply.github.com>
2023-04-12 11:14:38 +0700

    class -7

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-let name = prompt("Nhập tên của bạn:");
-if (name) {
+function getName() {
+    let name = prompt("Nhập tên của bạn:");
+    if (name) {
     alert("Xin chào " + name + "! Chào mừng bạn đã truy cập trang web này.");
-} else {
+    } else {
     alert("Xin chào! Chào mừng bạn đã truy cập trang web này.");
-}
-
-let color = prompt("Nhập tên màu bạn muốn đổi background:");
-if (color) {
+    }
+    }
+    
+function getBGColor() {
+    let color = prompt("Nhập tên màu bạn muốn đổi background:");
+    if (color) {
     document.body.style.backgroundColor = color;
-} else {
-    document.body.style.backgroundColor = "white";  
-}
+    } else {
+    document.body.style.backgroundColor = "white";
+    }
+    }
+    
+getName();
+getBGColor();
