81eabb0019d3c648820ec1fda1c7203464713461
duc-nq <108884348+duc-nq@users.noreply.github.com>
2023-04-13 11:44:01 +0700

    class 8

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 function getName() {
-    let name = prompt("Nhập tên của bạn:");
-    if (name) {
+    let name;
+    do {
+        name = prompt("Vui lòng nhập tên của bạn:");
+    } while (!name);
     alert("Xin chào " + name + "! Chào mừng bạn đã truy cập trang web này.");
-    } else {
-    alert("Xin chào! Chào mừng bạn đã truy cập trang web này.");
-    }
-    }
-    
+}
+
 function getBGColor() {
-    let color = prompt("Nhập tên màu bạn muốn đổi background:");
+    let color;
+    do {
+        color = prompt("Nhập tên màu bạn muốn đổi background:");
+    } while (!color);
     if (color) {
-    document.body.style.backgroundColor = color;
+        document.body.style.backgroundColor = color;
     } else {
-    document.body.style.backgroundColor = "white";
+        document.body.style.backgroundColor = "white";
     }
+}
+
+function getStarRating() {
+    let rating = prompt('What star rating would you give, 1-5?');
+    let starRating = parseInt(rating);
+
+    for (let i = 1; i <= starRating; i++) {
+        document.getElementById("star" + i).style.color = "yellow";
     }
-    
+}
+
 getName();
 getBGColor();
+getStarRating();
